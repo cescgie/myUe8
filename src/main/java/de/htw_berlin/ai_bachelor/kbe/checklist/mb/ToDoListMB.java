21ca684a19557554cad9b60e216f9cc8e55f51f4
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-06-18 15:39:14 +0200

    8.2.c) PhaseListener

--- src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/ToDoListMB.java
+++ src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/ToDoListMB.java
@@ -4,16 +4,23 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
 
 import de.htw_berlin.ai_bachelor.kbe.checklist.model.ToDo;
 import de.htw_berlin.ai_bachelor.kbe.checklist.model.ToDoList;
 
 @ManagedBean(name = "todoList", eager = true)
-public class ToDoListMB implements Serializable {
+public class ToDoListMB implements PhaseListener, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
     private ToDoList toDoList;
+    
+    FacesContext context = null;
+    String name;
     
 	public ToDoListMB() {
 		super();
@@ -37,4 +44,43 @@ public class ToDoListMB implements Serializable {
     public String save() {
     	return "save";
     }
+    
+    public String getName() {
+    return name;
+    }
+
+    public void setName(String name) {
+    this.name = name;
+    }
+
+    public void beforePhase(PhaseEvent event) {
+    	if (event.getPhaseId() == PhaseId.RESTORE_VIEW)
+		{
+			System.out.println("- RenderResponse "+event.getPhaseId()+ "\n");
+		}
+    	System.out.println("Before : - " + event.getPhaseId()); 
+    }
+    
+    public void afterPhase(PhaseEvent event) {
+    	System.out.println("After : - "+event.getPhaseId()+ "\n");
+    		if (event.getPhaseId() == PhaseId.RENDER_RESPONSE) 
+    		{
+    			if (FacesContext.getCurrentInstance().isPostback())
+    			{
+    				System.out.println("- PostbackRequest "+event.getPhaseId()+ "\n");
+    			}
+    		}
+    		
+    } 
+    
+    public PhaseId getPhaseId() {
+    return PhaseId.ANY_PHASE;
+    }
+    
+    /*
+     *Quelle :
+     *http://www.jroller.com/cschalk/entry/getting_familiar_with_the_jsf 
+     *http://stackoverflow.com/questions/8388854/how-to-implement-a-phaselistener-which-runs-at-end-of-lifecycle
+     *http://www.devmanuals.com/tutorials/java/jsf/jsf2TagLibrary/core/phaseListener.html
+     */
 }
